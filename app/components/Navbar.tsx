fa2d6b797af4cace112b3f65ea46aeda9c4bc1d6
unknown <bhardwajdev269@gmail.com>
2025-12-19 11:38:36 +0530

    navbar route protected using puterjs

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import { ToolCase } from 'lucide-react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router'
 import { Button } from '~/components/ui/button'
+import { usePuterStore } from '~/lib/puter'
 
 const Navbar = () => {
+  const {auth} = usePuterStore();
   return (
     <nav  className='fixed bg-white top-0 left-0 w-full shadow-md z-40 p-2 h-16 '>
         <div className='flex justify-between items-center'>
@@ -11,15 +13,50 @@ const Navbar = () => {
                <ToolCase className='icon'/>
                <li className='font-bold'>Content IQ</li> 
             </ul>
-            <ul className='flex items-center gap-4'>
+            {/* <ul className='flex items-center gap-4'>
                 <Link to="/">Home</Link>
                 <Link to="/pricing">Pricing</Link>
                 <Link to="/contact">Contact</Link>
+            </ul> */}
+            {
+              auth.isAuthenticated? (
+                <ul className='flex items-center gap-4'>
+                  <Link to="/">Home</Link>
+                  <Link to="/pricing">Pricing</Link>
+                  <Link to="/contact">Contact</Link>
+                  <Link to="/upload">Upload</Link>
+                  <Link to="/history">History</Link>
             </ul>
-            <ul className='flex  items-center gap-2'>
+              ): (
+                <ul className='flex items-center gap-4'>
+                  <Link to="/">Home</Link>
+                  <Link to="/pricing">Pricing</Link>
+                  <Link to="/contact">Contact</Link>
+                </ul>
+              )
+            }
+            {/* <ul className='flex  items-center gap-2'>
+              {auth.isAuthenticated ? (
+                <Link to="/auth">Sign In</Link>
+                <Button className='px-2 py-6'>Log Out</Button>
+              ):(
                 <Link to="/auth">Sign In</Link>
                 <Button className='px-2 py-6'>Get Started</Button>
-            </ul>
+              )}
+            </ul> */}
+            {
+              auth.isAuthenticated? (
+                <ul className='flex items-center gap-2'>
+                  <Button onClick={auth.signOut} className='px-2 py-6'>Log Out</Button>
+                </ul>
+              ): (
+                <ul className='flex items-center gap-2'>
+                  <Button className='px-2 py-6'>
+                    <Link to="/auth">Get Started</Link>
+                  </Button>
+                </ul>
+              )
+            }
         </div>
     </nav>
   )
